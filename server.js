70669d3bfb70f16b2bba09436aa92b19842ba4e0
Ritikraj11 <160333960+Ritikraj11@users.noreply.github.com>
2025-07-26 11:14:38 +0530

    Updated

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://gecms.vercel.app',
+  methods: ['POST'],
+  credentials: true
+}));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
